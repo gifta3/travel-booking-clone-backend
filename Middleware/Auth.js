9940a9d85c370d63fb4b8eb4e77e2a8b8bfcc0b5
gifta3 <iaravind499@gmail.com>
2026-06-14 05:04:59 -0700

    fixed auth

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -4,7 +4,7 @@ const auth=(req,res,next)=>{
 const token=req.headers.authorization;
 console.log(token);
 if(token){
-    const ogtoken=auth.split[1];
+    const ogtoken=auth.split(" ")[1];
     const data = jwt.verify(ogtoken,process.env.JWT_SECRET);
     console.log(data);
     req.user=data;
